e4a3af5d58f42278cfa1ccf3a9d8f428a2650fc6
oscarnar <orosas58@gmail.com>
2018-10-30 12:26:05 -0500

    Add files via upload

---
+++ hayka_run.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+
+#yes | sudo pacman -Sy
+
+#yes | sudo pacman -S libbsd qt5-base tmux --needed
+
+
+git clone https://github.com/tom-sb/HAYKA_proyect.git
+
+HAYKA=$HOME/HAYKA_proyect/
+
+cd $HAYKA
+
+g++ -std=c++11 test_hayka.cxx -o test -lbsd
+gcc servidor_hayka.c -o server
+
+
+cd $HAYKA/hayra
+
+qmake hayra.pro
+make
+
+
+
+
+
